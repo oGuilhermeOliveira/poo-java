e817219f136d07d3cbebf9ad93a8eb7c5676f3ff
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2025-06-11 23:03:04 -0300

    pilar encapsulamento

---
+++ Encapsulamento/src/ComputadorPedrinho.java
@@ -0,0 +1,9 @@
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNMessager msnMessenger = new MSNMessager();
+        msnMessenger.enviarMensagem();
+        msnMessenger.receberMensagem();
+        // msnMessenger.validarConectadoInternet();
+        // msnMessenger.salvarHistoricoMensagem();
+    }
+}
---
+++ Encapsulamento/src/MSNMessager.java
@@ -0,0 +1,21 @@
+public class MSNMessager {
+    public void enviarMensagem() {
+        if(validarConectadoInternet() )
+            System.out.println("Enviando mensagem");
+        else
+            System.out.println("Computador offline");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    private boolean validarConectadoInternet() {
+        System.out.println("Validando se está conectado a internet");
+        return false;
+    }
+
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
